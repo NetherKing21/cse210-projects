d1a7644f4744a0f9a900597786b4f761f700b925
NetherKing21 <braydenmcallister02@gmail.com>
2024-04-03 00:22:11 -0600

    Basic set up for In, Out, and the start screen

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,11 +1,39 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
+    static bool quitGame = false;
+    static int userInput = 0;
     static void Main(string[] args)
     {
-        
+        while (!quitGame)
+        {
+            bmRunGame();
+        }
     }
 
-    
+    static void bmRunGame()
+    {
+        bmStartScreen();
+    }
+
+    static void bmStartScreen()
+    {
+        Out.bmDisplayStartScreen();
+        userInput = In.bmGetUserInput(3);
+        if(userInput == 1)
+        {
+            // Load Game State
+        }
+        else if (userInput == 2)
+        {
+            // Start/Create New Game
+        }
+        else if (userInput == 3)
+        {
+            // Quit Game
+            quitGame = true;
+        }
+    }
 }
